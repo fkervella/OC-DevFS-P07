77f2ba0c24227cab8d222bd155f55423fa5f768e
François KERVELLA <kervella.francois@gmail.com>
2026-03-11 22:33:11 +0100

    Structuration des layouts

--- src/app/(wHeader)/layout.js
+++ src/app/(wHeader)/layout.js
@@ -1,7 +1,12 @@
+import Footer from '../_components/Footer';
+import Header from '../_components/Header';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <Header />
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
