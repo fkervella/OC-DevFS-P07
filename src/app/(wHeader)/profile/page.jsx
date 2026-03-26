f714aeab055acd546af8abeb66ddf87c1f953544
François KERVELLA <kervella.francois@gmail.com>
2026-03-26 21:38:01 +0100

    Application de la structure client/server à la page profile

--- src/app/(wHeader)/profile/page.jsx
+++ src/app/(wHeader)/profile/page.jsx
@@ -1,60 +1,18 @@
-import BlackButton from '@/app/_components/Common/BlackButton';
-import LabelInput from '@/app/_components/Common/LabelInput';
 import { getUserProfile } from '@/app/actions/profile';
 
+import ProfileClient from './pageClient.jsx';
+
 /**
- * Profile Composant d'afficahge des informations de profil utilisateur
+ * ProfileServer Partie serveur d'afficahge des informations de profil utilisateur
  *
  * @async
- * @returns {string} Code HTML d'afficahge des informations de profil utilisateur
+ * @returns {ProfileClient} Partie client pour l'afficahge des informations de profil utilisateur
  */
 
-async function Profile() {
+async function ProfileServer() {
   const { user } = await getUserProfile();
-  const username = user.name.split(' ');
 
-  const firstname = username[0];
-  const lastname = username[1];
-
-  return (
-    <div className="flex flex-col gap-4 mt-4 pt-10 pr-30 pb-10 pl-30 bg-background">
-      <div className=" bg-white pt-10 pr-20 pb-10 pl-20 rounded-lg">
-        <div className="text-manrope font-semibold text-lg text-black">
-          Mon compte
-        </div>
-        <div className="text-inter font-normal text-base text-grey-font">
-          {user.name}
-        </div>
-        <form className="flex flex-col gap-2 w-full">
-          <LabelInput
-            name="nom"
-            text="Nom"
-            type="text"
-            placeHolder={firstname}
-          />
-          <LabelInput
-            name="prenom"
-            text="Prénom"
-            type="text"
-            placeHolder={lastname}
-          />
-          <LabelInput
-            name="email"
-            text="Email"
-            type="email"
-            placeHolder={user.email}
-          />
-          <LabelInput
-            name="password"
-            text="Mot de passe"
-            type="password"
-            placeHolder="***"
-          />
-          <BlackButton text="Modifier les informations" />
-        </form>
-      </div>
-    </div>
-  );
+  return <ProfileClient user={user} />;
 }
 
-export default Profile;
+export default ProfileServer;
