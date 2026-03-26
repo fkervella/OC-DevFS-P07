040662eebe97058adc2b496613f7a8d7f79e9283
François KERVELLA <kervella.francois@gmail.com>
2026-03-26 15:46:01 +0100

    Correction de l'affichage du composant TaskStatus

--- src/app/_components/Task/TaskStatus/index.jsx
+++ src/app/_components/Task/TaskStatus/index.jsx
@@ -31,7 +31,7 @@ function TaskStatus({ status }) {
 
   return (
     <div
-      className={`flex pt-1 pr-4 pb-1 pl-4 ${statusStyle[status].bgColor} ${statusStyle[status].textColor} rounded-full h-full text-sm font-normal items-center w-fit`}
+      className={`flex pt-1 pr-4 pb-1 pl-4 ${statusStyle[status].bgColor} ${statusStyle[status].textColor} rounded-full text-sm font-normal items-center w-fit h-fit text-nowrap`}
     >
       {statusStyle[status].text}
     </div>
